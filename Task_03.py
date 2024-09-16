1f3099c2a84b7fe62788ddee76ed92088b4b177e
Angel <sonarobert1006@gmail.com>
2024-09-16 17:34:28 +0530

    Update Task_03.py

--- Task_03.py
+++ Task_03.py
@@ -1,3 +1,4 @@
+import string
 from tkinter import *
 import re
 
@@ -9,28 +10,46 @@ var=StringVar()
 regex=re.compile("[!@#$%^&*~]")
 def password_checker():
     word=entry.get(1.0,"end-1c")
-    if len(word)<8:
-        var.set("Password must be atleast 8 characters!!")
-    elif not re.search("[A-Z]", word):
-        var.set("Password must contain atleast one Uppercase letter!!")
-    elif not re.search("[a-z]", word):
-        var.set("Password must contain atleast one Lowercase letter!!")
-    elif not re.search("[0-9]", word):
-        var.set("Password must contain atleast one Number!!")
-    elif not regex.search(word):
-        var.set("Password must contain atleast one special Character!!")
-    else:
-        var.set("                          Password is Strong!!            ")
+    upper_case=any([1 if c in string.ascii_uppercase else 0 for c in word])
+    lower_case=any([1 if c in string.ascii_lowercase else 0 for c in word])
+    special=any([1 if c in string.punctuation else 0 for c in word])
+    digits=any([1 if c in string.digits else 0 for c in word])
+    characters=[upper_case,lower_case, special, digits]
+    #print(sum(characters))
+    length=len(word)
+    score=0
+    if length>8:
+        score+=1
+    if length>12:
+        score+=1
+    if length>17:
+        score+=1
+    if length>20:
+        score+=1
+    if sum(characters)>1:
+        score+=1
+    if sum(characters)>2:
+        score+=1
+    if sum(characters)>3:
+        score+=1
+    if score<4:
+        var.set("WEAK PASSWORD")
+    elif score==4:
+        var.set("MODERATE PASSWORD")
+    elif score>4:
+        var.set("STRONG PASSWORD")
+    
     
 lf=Label(root, text="Enter the Password")
 lf.place(x=140,y=10)
 
-entry=Text(root,height=1,width=10)
-entry.place(x=150,y=40)
+entry=Text(root,height=1,width=20)
+entry.place(x=100,y=40)
 
 b1=Button(root,text="check", command=password_checker)
 b1.place(x=170,y=70)
 
 l=Label(root, textvariable=var)
-l.place(x=70,y=100)
-root.mainloo()
+l.place(x=130,y=150)
+
+root.mainloop()
